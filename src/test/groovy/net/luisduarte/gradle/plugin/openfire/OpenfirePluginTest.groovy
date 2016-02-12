acb363afcda378139f61464ef77de09fc773255e
Luís Duarte <lduarte@luisduarte.net>
2016-02-12 11:05:05 +0000

    Add Nebula Test framework. Refactored Tests

--- src/test/groovy/net/luisduarte/gradle/plugin/openfire/OpenfirePluginTest.groovy
+++ src/test/groovy/net/luisduarte/gradle/plugin/openfire/OpenfirePluginTest.groovy
@@ -1,19 +1,11 @@
 package net.luisduarte.gradle.plugin.openfire
 
-import org.gradle.api.Project
-import org.gradle.testfixtures.ProjectBuilder
-import org.junit.Test
-
+import nebula.test.PluginProjectSpec
 /**
  * Created by lduarte on 18/01/16.
  */
-class OpenfirePluginTest {
+class OpenfirePluginTest extends PluginProjectSpec {
 
-    @Test
-    public void openfirePluginProject() {
-        Project project = ProjectBuilder.builder().build()
-        project.pluginManager.apply 'net.luisduarte.openfire'
-
-        assert project.version != null
-    }
+    @Override
+    String getPluginName() { return 'net.luisduarte.openfire' }
 }
